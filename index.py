1d84c84e8677a2bef915e0ff9108de6a01b5841b
Aniuddh Pawar <53261484+Aniruddh6@users.noreply.github.com>
2023-03-11 13:30:40 +0530

    dgf

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Mala whatsapp kar na, fakt 5 min bolu, fakt 5 min"
+    return "Mala whatsapp kar na, fakt 5 min bolu, fakt 5 min, samja aata possibble hot nasel tar mala ek time sang ki mi ya timela free asen tevha mi message karen tevha bolu 5 min, please he sang karan kal mi 2 paryant ratri thamblelo ki hila aata tari jamel bolayla karan tula 11 vajta possible navta"
 
 
 if __name__ == "__main__":
